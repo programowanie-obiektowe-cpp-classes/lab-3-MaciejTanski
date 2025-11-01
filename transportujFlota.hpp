c613e214c8e69ce11858eb68d05c434c1b09fd7b
MaciejTanski <tanior0803@gmail.com>
2025-11-01 18:01:59 +0100

    /dziala

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,22 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int przewieziony=0;
+    unsigned int zaglowce=0;
+
+    while (przewieziony<towar){
+        Statek* Lajba=stocznia();
+
+        if(dynamic_cast<Zaglowiec*>(Lajba))
+            zaglowce++;
+
+        auto ladunek=Lajba->transportuj();
+        przewieziony+=ladunek;
+        delete Lajba;
+     }
+
+
+
+    return zaglowce;
 }
